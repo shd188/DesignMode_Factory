9f4064cba0bd4cf4f4bca80da20966696d55f367
shd188 <zs.shd15@gmail.com>
2015-09-11 16:23:24 +0800

    Update MainActivity.java

--- app/src/main/java/com/aimer/shd/designmode_factory/MainActivity.java
+++ app/src/main/java/com/aimer/shd/designmode_factory/MainActivity.java
@@ -9,6 +9,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Operation oper = OperationFactory.createOperation("/");
+        oper.numA = 5;
+        oper.numB = 3;
+        double result ;
+        try {
+            result = oper.getResult();
+            Log.d("Factory", "结果是：" + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("Factory", "结果是：" + e.toString());
+        }
     }
 
 
